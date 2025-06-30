f06aeb470dfac9c14159cdca520fcb3d55a654ef
Raghav Sachdev <iraghavsachdev@gmail.com>
2025-06-30 19:05:49 +0530

    Fixes

--- app/components/HomePageClient.tsx
+++ app/components/HomePageClient.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { VideoPlayer } from './VideoPlayer';
 import dynamic from 'next/dynamic';
 import { motion, Variants } from 'framer-motion';
