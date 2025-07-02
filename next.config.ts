db759103519cf1506f9415727ac9ac4e7d181f20
Dhruvi <dhruvi_2006@Mac.mshome.net>
2025-07-02 13:09:27 +0530

    koi bhi msg

--- next.config.ts
+++ next.config.ts
@@ -106,7 +106,6 @@ const nextConfig = {
     
     // Experimental features for better performance
     experimental: {
-        optimizeCss: true,
         optimizePackageImports: ['framer-motion'],
     },
     
